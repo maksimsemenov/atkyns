c0116921a86162f4e9d77986ae849ddb0fee5fdf
maksimsemenov <maksa.semenov@gmail.com>
2016-09-26 11:39:53 -0400

    Empty state

--- src/components/EmptyCases/EmptyCases.js
+++ src/components/EmptyCases/EmptyCases.js
@@ -1,22 +1,23 @@
 import React from 'react'
 import l from 'utils/local'
+import Start from './Start'
 import Print from './Print'
 import Fill from './Fill'
 import './EmptyCases.less'
 
 const EmptyCases = () => (
   <div className='emptyCases'>
-    <div className='emptyCases__step'>
-      <div className='emptyCases__stepImage'></div>
+    <div className='emptyCases__step emptyCases__stepOne'>
+      <div className='emptyCases__stepImage'><Start /></div>
       <h2 className='emptyCases__stepTitle'>{l('%emptyStepOneTitle')}</h2>
       <p className='emptyCases__stepText'>{l('%emptyStepOneText')}</p>
     </div>
-    <div className='emptyCases__step'>
+    <div className='emptyCases__step emptyCases__stepTwo'>
       <div className='emptyCases__stepImage'><Fill /></div>
       <h2 className='emptyCases__stepTitle'>{l('%emptyStepTwoTitle')}</h2>
       <p className='emptyCases__stepText'>{l('%emptyStepTwoText')}</p>
     </div>
-    <div className='emptyCases__step'>
+    <div className='emptyCases__step emptyCases__stepThree'>
       <div className='emptyCases__stepImage'><Print /></div>
       <h2 className='emptyCases__stepTitle'>{l('%emptyStepThreeTitle')}</h2>
       <p className='emptyCases__stepText'>{l('%emptyStepThreeText')}</p>
--- src/components/EmptyCases/Fill.js
+++ src/components/EmptyCases/Fill.js
@@ -2,28 +2,24 @@ import React from 'react'
 import './Fill.less'
 
 const Fill = () => (
-  <svg className='fillImg' viewBox='0 0 152 122' fill='none'>
-    <rect className='fillImg__screen fillImg__aFadeIn' x='1' y='1' width='150' height='120' rx='2' />
-
-    <path className='fillImg__windowFrame fillImg__aStrokeIn' id='fill-w-1' d='M148.992981,1 C150.101427,1 151,1.89691639 151,3.00751708 L151,118.992483 C151,120.101204 150.100155,121 148.992981,121 L3.00701877,121 C1.89857291,121 1,120.103084 1,118.992483' />
-    <path className='fillImg__windowFrame fillImg__aStrokeIn' id='fill-w-2' d='M3.00701877,121 C1.89857291,121 1,120.103084 1,118.992483 L1,3.00751708 L1,3.00751708 C1,1.89879601 1.89984519,1 3.00701877,1 L148.992981,1 C150.101427,1 151,1.89691639 151,3.00751708' />
-    <path className='fillImg__windowFrame fillImg__aStrokeIn' id='fill-w-3' d='M1,11 L151,11' />
-    <circle className='fillImg__browserBtn fillImg__aScaleIn' id='fill-b-1' cx='7' cy='6' r='2' />
-    <circle className='fillImg__browserBtn fillImg__aScaleIn' id='fill-b-2' cx='13' cy='6' r='2' />
-    <circle className='fillImg__browserBtn fillImg__aScaleIn' id='fill-b-3' cx='19' cy='6' r='2' />
-
-    <rect className='fillImg__fieldFrame fillImg__aStrokeIn' id='fill-f-1' x='44' y='26' width='57' height='10' rx='1' />
-    <rect className='fillImg__fieldText fillImg__aScaleIn' id='fill-t-1' x='48' y='30' width='31' height='2' />
-    <path className='fillImg__text fillImg__aScaleIn' id='fill-t-2' d='M44,39 L99,39 L99,41 L44,41 L44,39 Z M44,43 L91,43 L91,45 L44,45 L44,43 Z M44,47 L59,47 L59,49 L44,49 L44,47 Z' />
-    <path className='fillImg__checkmark fillImg__aScaleIn' id='fill-c-1' d='M113.565,29.122 C112.467,30.103 111.189,31.939 110.406,33.226 C109.794,32.452 109.047,31.714 108.354,31.012 L108.831,30.544 L110.091,31.651 C110.721,31.012 111.36,30.391 112.026,29.797 C112.359,29.5 112.863,29.032 113.25,28.807 C113.376,28.798 113.529,29.05 113.565,29.122 L113.565,29.122 Z M115.5,30.787 C115.5,28.303 113.484,26.287 111,26.287 C108.516,26.287 106.5,28.303 106.5,30.787 C106.5,33.271 108.516,35.287 111,35.287 C113.484,35.287 115.5,33.271 115.5,30.787 L115.5,30.787 Z' />
-
-    <rect className='fillImg__fieldFrame fillImg__aStrokeIn' id='fill-f-2' x='44' y='59' width='57' height='10' rx='1' />
-    <path className='fillImg__fieldText fillImg__aScaleIn' id='fill-t-3' d='M48,63 L67,63 L67,65 L48,65 L48,63 Z M74,63 L85,63 L85,65 L74,65 L74,63 Z' />
-    <path className='fillImg__text fillImg__aScaleIn' id='fill-t-4' d='M44,72 L99,72 L99,74 L44,74 L44,72 Z M44,76 L79,76 L79,78 L44,78 L44,76 Z' />
-    <path className='fillImg__checkmark fillImg__aScaleIn' id='fill-c-2' d='M113.565,62.122 C112.467,63.103 111.189,64.939 110.406,66.226 C109.794,65.452 109.047,64.714 108.354,64.012 L108.831,63.544 L110.091,64.651 C110.721,64.012 111.36,63.391 112.026,62.797 C112.359,62.5 112.863,62.032 113.25,61.807 C113.376,61.798 113.529,62.05 113.565,62.122 L113.565,62.122 Z M115.5,63.787 C115.5,61.303 113.484,59.287 111,59.287 C108.516,59.287 106.5,61.303 106.5,63.787 C106.5,66.271 108.516,68.287 111,68.287 C113.484,68.287 115.5,66.271 115.5,63.787 L115.5,63.787 Z' />
-
-    <rect className='fillImg__btn fillImg__aScaleIn' id='fill-b-4'  x='57' y='91' width='30' height='10' rx='1' />
-    <rect className='fillImg__btnText fillImg__aScaleIn' id='fill-t-5' x='66' y='95' width='12' height='2' />
+  <svg className='fillImg' viewBox='0 0 142 152' fill='none'>
+    <rect fill='#FFFFFF' x='1' y='1' width='140' height='150' rx='2'></rect>
+    <path d='M44,26 L98,26 L98,30 L44,30 L44,26 Z M49,34 L93,34 L93,38 L49,38 L49,34 Z' fill='#6E6862'></path>
+    <rect fill='#4EA466' x='56' y='118' width='30' height='10' rx='1'></rect>
+    <rect fill='#FFFFFF' x='65' y='122' width='12' height='2'></rect>
+    <path d='M43,66 L98,66 L98,68 L43,68 L43,66 Z M43,70 L90,70 L90,72 L43,72 L43,70 Z M43,74 L58,74 L58,76 L43,76 L43,74 Z' fill='#B7B3B1'></path>
+    <rect fill='#4EA466' x='47' y='57' width='31' height='2'></rect>
+    <rect stroke='#4EA466' strokeWidth='0.5' x='43' y='53' width='57' height='10' rx='1'></rect>
+    <path d='M95.67,59.142 C95.33,58.712 94.915,58.302 94.53,57.912 L94.795,57.652 L95.495,58.267 C95.845,57.912 96.2,57.567 96.57,57.237 C96.755,57.072 97.035,56.812 97.25,56.687 C97.32,56.682 97.405,56.822 97.425,56.862 C96.815,57.407 96.105,58.427 95.67,59.142 Z M96,55.287 C94.62,55.287 93.5,56.407 93.5,57.787 C93.5,59.167 94.62,60.287 96,60.287 C97.38,60.287 98.5,59.167 98.5,57.787 C98.5,56.407 97.38,55.287 96,55.287 Z' id='ó' fill='#4EA466'></path>
+    <path d='M43,99 L98,99 L98,101 L43,101 L43,99 Z M43,103 L78,103 L78,105 L43,105 L43,103 Z' fill='#B7B3B1'></path>
+    <path d='M47,90 L66,90 L66,92 L47,92 L47,90 Z M73,90 L84,90 L84,92 L73,92 L73,90 Z' fill='#4EA466'></path>
+    <rect stroke='#4EA466' strokeWidth='0.5' x='43' y='86' width='57' height='10' rx='1'></rect>
+    <path d='M95.67,92.142 C95.33,91.712 94.915,91.302 94.53,90.912 L94.795,90.652 L95.495,91.267 C95.845,90.912 96.2,90.567 96.57,90.237 C96.755,90.072 97.035,89.812 97.25,89.687 C97.32,89.682 97.405,89.822 97.425,89.862 C96.815,90.407 96.105,91.427 95.67,92.142 Z M96,88.287 C94.62,88.287 93.5,89.407 93.5,90.787 C93.5,92.167 94.62,93.287 96,93.287 C97.38,93.287 98.5,92.167 98.5,90.787 C98.5,89.407 97.38,88.287 96,88.287 Z' id='ó' fill='#4EA466'></path>
+    <rect stroke='#6E6862' x='1' y='1' width='140' height='150' rx='2'></rect>
+    <circle fill='#6E6862' cx='7' cy='6' r='2'></circle>
+    <ellipse fill='#6E6862' cx='13' cy='6' rx='2' ry='2'></ellipse>
+    <circle fill='#6E6862' cx='19' cy='6' r='2'></circle>
+    <path d='M1,10.25 L141,10.25' id='Line' stroke='#6E6862' strokeWidth='0.5' strokeLinecap='square'></path>
   </svg>
 )
 
