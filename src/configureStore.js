d7372642962d4090d33d7c913fd2a1eb87cedd27
maksimsemenov <maksa.semenov@gmail.com>
2016-10-01 09:39:38 -0400

    Add middleware
    Add logger and thunk middleware to the project. Logger middleware
    active only in development

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,12 +1,27 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { fromJS } from 'immutable'
 import throttle from 'lodash/throttle'
+import createLogger from 'redux-logger'
+import thunk from 'redux-thunk'
 import { loadState, saveState } from 'localStorage'
 import reducer from 'reducers'
 
+let middleware = [thunk]
+
+if (process.env.NODE_ENV === 'development') {
+  const logger = createLogger({
+    stateTransformer: (state) => state.toJS()
+  })
+  middleware.push(logger)
+}
+
 const configureStore = () => {
   const persistedState = fromJS({ cases: loadState() })
-  const store = createStore(reducer, persistedState)
+  const store = createStore(
+    reducer,
+    persistedState,
+    applyMiddleware(...middleware)
+  )
 
   store.subscribe(throttle(() => {
     saveState(store.getState().get('cases').toJS())
