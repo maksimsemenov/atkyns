7a5e68f333b3c0f1744b70d6b8bf501cb3bbc270
maksimsemenov <maksa.semenov@gmail.com>
2016-10-28 22:17:36 -0400

    Add textfield and related tests

--- src/components/Textfield/Textfield.js
+++ src/components/Textfield/Textfield.js
@@ -1,33 +1,86 @@
-import React, { Component, PropTypes } from 'react'
+import React, { PropTypes } from 'react'
+import Autocomplete from 'react-autocomplete'
+import './Textfield.less'
 
-class Textfield extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { value: '' }
-  }
-  handleChange(e) {
 
+// Standart helper functions for autocomplete
+export const renderMenu = (items, value, style) =>
+  items ?
+  <ul className='textfield-ac' children={items} style={style} /> :
+  null
+
+export const renderItem = (item, isHighlighted) =>
+  <li
+    key={item.value}
+    className={`textfield-acItem${isHighlighted ? 'is-highlighted' : ''}`}
+  >{item.name}</li>
+
+export const getItemValue = item => item.name
+export const shouldItemRender = (item, value) =>
+  item.value.toLowerCase().search(value.toLowerCase()) !== -1
+
+const defaultAC = {
+  renderItem,
+  renderMenu,
+  getItemValue,
+  shouldItemRender
+}
+
+// Textfield component itself
+const Textfield = ({
+  value,
+  mask,
+  wrapperStyle,
+  appearance,
+  autocomplete,
+  onChange,
+  error,
+  note,
+  ...rest
+}) => {
+  const maskedValue = mask ? mask(value) : value
+  const ac = autocomplete ? {...defaultAC, ...autocomplete } : undefined
+  const inputProps = {
+    className: `textfield${error ? ' has-error' : ''}`,
+    ...rest
   }
-  render() {
-    return (
-      <input
-        type='text' />
-    )
-  }
+  return (
+    <div className='textfield__control' style={wrapperStyle}>
+      {ac ? (
+        <Autocomplete
+          value={maskedValue}
+          onSelect={onChange}
+          onChange={(event, value) => onChange(value)}
+          inputProps={inputProps}
+          {...ac}
+        />
+      ) : (
+        <input value={maskedValue} onChange={onChange} {...inputProps} />
+      )}
+      {error && <div className='ob-textfield-error'>{error}</div>}
+      {!error && note && <div className='ob-textfield-note'>{note}</div>}
+    </div>
+  )
 }
 
 Textfield.propTypes = {
   value: PropTypes.string,
-  type: PropTypes.oneOf(['']),
+  type: PropTypes.oneOf(['text', 'email', 'password', 'url', 'date', 'number']),
+  wrapperStyle: PropTypes.object,
+  autocomplete: PropTypes.shape({
+    items: PropTypes.arrayOf(PropTypes.shape({
+      value: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired
+    })).isRequired,
+    shouldItemRender: PropTypes.func
+  }),
+  mask: PropTypes.func,
+  onChange: PropTypes.func.isRequired,
+
   appearance: PropTypes.oneOf(['default', 'inGroup']),
-  length: PropTypes.string,
-  autocomplete: PropTypes.func,
-  placeholder: PropTypes.string,
-  onChange: PropTypes.func.isRequired
 }
 Textfield.defaultProps = {
-  type: '',
-  length: '200px',
+  type: 'text',
 }
 
 export default Textfield
--- src/components/Textfield/Textfield.test.js
+++ src/components/Textfield/Textfield.test.js
@@ -1,11 +1,65 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import Textfield from 'components/Textfield/Textfield'
+import renderer from 'react-test-renderer'
+import Textfield, * as fromTextfield from 'components/Textfield/Textfield'
+jest.mock('react-dom')
 
 describe('Textfield component', () => {
-  it('renders without craches', () => {
-    const div = document.createElement('div')
-    const onChange = () => {}
-    ReactDOM.render(<Textfield onChange={onChange} />, div)
+  it('Snapshot test', () => {
+    const component0 = renderer.create(<Textfield onChange={() => {}} />)
+    expect(component0.toJSON()).toMatchSnapshot()
+
+    const component1 = renderer.create(
+      <Textfield
+        onChange={() => {}}
+        note='Note for the field'
+      />
+    )
+    expect(component1.toJSON()).toMatchSnapshot()
+
+    const component2 = renderer.create(
+      <Textfield
+      onChange={() => {}}
+      error='Error in field'
+      />
+    )
+    expect(component2.toJSON()).toMatchSnapshot()
+
+    const component3 = renderer.create(
+      <Textfield
+        onChange={() => {}}
+        value='Test value'
+        autocomplete={{
+          items: [
+            { value: 'item1', name: 'item1' },
+            { value: 'item2', name: 'item2' },
+            { value: 'item3', name: 'item3' }
+          ]
+        }}
+      />
+    )
+    expect(component3.toJSON()).toMatchSnapshot()
   })
+})
+
+describe('renderMenu function', () => {
+  it('Snapshot test', () => {
+    const tree = renderer.create(fromTextfield.renderMenu(['item1', 'item2']))
+    expect(tree.toJSON()).toMatchSnapshot()
+  })
+})
+describe('renderItem function', () => {
+  it('Snapshot test', () => {
+    const tree1 = renderer.create(fromTextfield.renderItem({ value: 'item1', name: 'item2' }, false))
+    expect(tree1.toJSON()).toMatchSnapshot()
+    const tree2 = renderer.create(fromTextfield.renderItem({ value: 'item1', name: 'item2' }, true))
+    expect(tree2.toJSON()).toMatchSnapshot()
+  })
+})
+it('shouldItemRender works correctly', () => {
+  expect(fromTextfield.shouldItemRender({ value: 'Test item'}, 'ite')).toBeTruthy()
+  expect(fromTextfield.shouldItemRender({ value: 'Test item'}, 'abr')).toBeFalsy()
+})
+it('getItemValue works correctly', () => {
+  expect(fromTextfield.getItemValue({ name: 'Albert' })).toEqual('Albert')
+  expect(fromTextfield.getItemValue({})).toBeUndefined()
 })
