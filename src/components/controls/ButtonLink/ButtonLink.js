6a9bfe98f9cde7f86c0c3ae8939939ee59f34559
maksimsemenov <maksa.semenov@gmail.com>
2016-11-16 20:37:04 -0500

    ButtonLink

---
+++ src/components/controls/ButtonLink/ButtonLink.js
@@ -0,0 +1,16 @@
+import React, { PropTypes } from 'react'
+import Link from 'react-router/Link'
+import './ButtonLink.less'
+
+const ButtonLink = ({ children, disabled, ...rest }) => (
+  <Link className={`button${disabled ? ' is-disabled' : ''}`} {...rest}>
+    {children}
+  </Link>
+)
+
+ButtonLink.propTypes = {
+  to: PropTypes.string.isRequired,
+  disabled: PropTypes.bool
+}
+
+export default ButtonLink
