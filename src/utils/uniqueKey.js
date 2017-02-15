341262ba243e60d717ec5dfa0e1e745f720519f8
maksimsemenov <maksa.semenov@gmail.com>
2017-02-14 21:48:28 -0500

    code formatting

--- src/utils/uniqueKey.js
+++ src/utils/uniqueKey.js
@@ -1,7 +1,9 @@
-const uniqueKey = (randomPartSize = 4) => {
-  const random = Math.random()*Math.pow(10, randomPartSize)
+// @flow
+
+const uniqueKey = (randomPartSize: number = 4) => {
+  const random = Math.random() * Math.pow(10, randomPartSize)
   const date = Date.now()
-  const key = date*Math.pow(10, randomPartSize)+(Math.trunc(random))
+  const key = date * Math.pow(10, randomPartSize) + (Math.trunc(random))
   return key.toString(36)
 }
 
