14eff434248f5a85f4e4508b800cc6e0fafb6ad4
maksimsemenov <maksa.semenov@gmail.com>
2017-02-18 15:34:41 -0500

    Clean up

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,22 +1,13 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import { fromJS } from 'immutable'
 import throttle from 'lodash/throttle'
-import createLogger from 'redux-logger'
 import thunk from 'redux-thunk'
 import { loadState, saveState } from 'localStorage'
 import reducer from 'reducers'
 
 let middleware = [thunk]
 
-if (process.env.NODE_ENV === 'development') {
-  const logger = createLogger({
-    stateTransformer: (state) => state.toJS()
-  })
-  middleware.push(logger)
-}
-
 const composeEnhancers =
-  process.env.NODE_ENV !== 'production' &&
   typeof window === 'object' &&
   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) :
